d4d507c7d78e03f2455c142e1963b379a519386e
Lionclaw49 <griffindugan49@gmail.com>
2017-08-08 21:10:57 -0400

    Fixed it

--- Visuals.py
+++ Visuals.py
@@ -1,37 +1,29 @@
 import wx
-
 class MyFrame(wx.Frame):
-	def __init__ (self):
-		
-		wx.Frame.__init__(self, None,
-			
-			pos=wx.DefaultPosition, #position
+    def __init__(self):
+        wx.Frame.__init__(self, None,
+            pos=wx.DefaultPosition, size=wx.Size(450, 100),
+            style=wx.MINIMIZE_BOX | wx.SYSTEM_MENU | wx.CAPTION |
+             wx.CLOSE_BOX | wx.CLIP_CHILDREN,
+            title="Tuesday")
+        panel = wx.Panel(self)
+        my_sizer = wx.BoxSizer(wx.VERTICAL)
+        lbl = wx.StaticText(panel,
+        label="Hi. I'm Tuesday. How can I help?")
+        my_sizer.Add(lbl, 0, wx.ALL, 5)
+        self.txt = wx.TextCtrl(panel, style=wx.TE_PROCESS_ENTER,size=(400,30))
+        self.txt.SetFocus()
+        self.txt.Bind(wx.EVT_TEXT_ENTER, self.OnEnter)
+        my_sizer.Add(self.txt, 0, wx.ALL, 5)
+        panel.SetSizer(my_sizer)
+        self.Show()
 
-			size=wx.Size(450, 100),
-			
-			style=wx.MINIMIZE_BOX | wx.SYSTEM_MENU | wx.CAPTION | wx.CLOSE_BOX | wx.CLIP_CHILDREN,
-			
-			title="Tuesday")
-		
-		panel = wx.Panel(self)
-		my_sizer = wx.BoxSizer(wx.VERTICAL)
-		lbl = wx.StaticText(panel,
-		label="Hello, I am Tuesday. How can I help?"
-		my_sizer.Add(lbl, 0, wx.ALL, 5)
-		self.txt = wx.TextCtrl(panel, style=wx.TE_PROCESS_ENTER,size=(400,30))
-		self.txt.SetFocus() #focus text
-		self.txt.Bind(wx.EVT_TEXT_ENTER, self.OnEnter)
-		my_sizer.Add(self.txt, 0, wx.ALL, 5)
-		panel.SetSizer(my_sizer)
-		self.Show()
+    def OnEnter(self, event):
+        input = self.txt.GetValue()
+        input = input.lower()
 
-	def OnEnter(self, event):
-		input = self.txt.GetValue()
-		input = input.lower()
-		print "It Worked!"
 
 if __name__ == "__main__":
-	app = wx.App(True)
-	frame = MyFrame()
-	app.MainLoop()
-		
+    app = wx.App(True)
+    frame = MyFrame()
+    app.MainLoop()
