3fbad61f8dd1d91caffe5dd82bc859c07232b0a8
Lionclaw49 <griffindugan49@gmail.com>
2017-08-29 16:06:51 -0400

    Hopefully this will give me a faster result

--- Tuesday.py
+++ Tuesday.py
@@ -6,18 +6,18 @@ while True:
     uinput = raw_input("Question: ")  #Adds user input
     
     try:
+        #WIKIPEDIA
+        #wikipedia.set_lang("en")  #Language!
+        uinput = uinput.split(' ')
+        uinput = " ".join(uinput[2:])
+        print wikipedia.summary(uinput)
+        
+    except: 
     #WOLFRAM ALPHA
         app_id = "G58JY9-WQ963T9EQV"  #to get the info
         client = wolframalpha.Client(app_id)  #connecting to info
         result = client.query(uinput)  #collecting result
         answer = next(result.results).text  #processing answer
         print answer  #answering with answer
-        
-    except: 
-        #WIKIPEDIA
-        #wikipedia.set_lang("en")  #Language!
-        uinput = uinput.split(' ')
-        uinput = " ".join(uinput[2:])
-        print wikipedia.summary(uinput)
         
         
