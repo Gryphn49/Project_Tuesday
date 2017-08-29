b55051fc6e220e6a4904094f07f83bd90e314a39
Lionclaw49 <griffindugan49@gmail.com>
2017-08-29 16:09:45 -0400

    Now in testing of providing an exit

--- Tuesday.py
+++ Tuesday.py
@@ -6,12 +6,16 @@ while True:
     uinput = raw_input("Question: ")  #Adds user input
     
     try:
-        #WIKIPEDIA
+        uinput = "exit program"
+        exit()
+        
+    except: 
+       #WIKIPEDIA
         #wikipedia.set_lang("en")  #Language!
         uinput = uinput.split(' ')
         uinput = " ".join(uinput[2:])
         print wikipedia.summary(uinput)
-        
+
     except: 
     #WOLFRAM ALPHA
         app_id = "G58JY9-WQ963T9EQV"  #to get the info
@@ -19,5 +23,6 @@ while True:
         result = client.query(uinput)  #collecting result
         answer = next(result.results).text  #processing answer
         print answer  #answering with answer
-        
+    
+    
         
