7be42ddc62b0ab6dfda7ba93a8f3aed4e22ae1c4
Lionclaw49 <griffindugan49@gmail.com>
2017-12-04 08:58:43 -0500

    corrected indents

--- Discord-Bot.py
+++ Discord-Bot.py
@@ -37,7 +37,7 @@ async def on_message(message):
         answer = wikipedia.summary(uinput, chars=1900)#shortening the code below
         await client.send_message(message.channel, answer)#sending answer
     
-  client.run("Your Token Here")
+client.run("Your Token Here")
   
   #On the Discord Developers website, you have to create an bot id for it. Then you get the token, paste it above. Then add it to your server!
   #Have fun!
