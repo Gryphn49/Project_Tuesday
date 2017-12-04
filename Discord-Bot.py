28c87b4b139202d5cdd1350cc5162e9cffe73072
Lionclaw49 <griffindugan49@gmail.com>
2017-12-04 09:01:14 -0500

    Added my token.

--- Discord-Bot.py
+++ Discord-Bot.py
@@ -37,8 +37,9 @@ async def on_message(message):
         answer = wikipedia.summary(uinput, chars=1900)#shortening the code below
         await client.send_message(message.channel, answer)#sending answer
     
-client.run("Your Token Here")
+client.run("Mzg2NzAwOTYxMDUxMDQ5OTg1.DQbmfA.GICszcU-1t_Xk6Dc7TE2pdnfC6w")
   
+  #I have my bot token there, so you would have to replace it with yours. The instructions for that are below..
   #On the Discord Developers website, you have to create an bot id for it. Then you get the token, paste it above. Then add it to your server!
   #Have fun!
                      
